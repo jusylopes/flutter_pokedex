eec102e2688a57c81aa54ef8393acc3b3654a470
jusylopes <jusylopes@outlook.com>
2022-11-05 19:37:22 -0300

    refactor routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex/cubit/pokemon/pokemon_cubit.dart';
 import 'package:pokedex/cubit/pokemon_species/pokemon_species_cubit.dart';
 import 'package:pokedex/services/pokemon_repository.dart';
-import 'package:pokedex/utils/routes.dart';
 import 'package:pokedex/utils/theme.dart';
-import 'package:pokedex/views/pokemon_detail/pokemon_detail_page.dart';
-import 'package:pokedex/views/pokemon_home/pokemon_home_page.dart';
-import 'package:pokedex/views/pokemon_splash/pokemon_splash_page.dart';
+import 'package:pokedex/views/pokemon_splash_page.dart';
 
 void main() {
   runApp(const Pokedex());
@@ -23,7 +20,7 @@ class Pokedex extends StatelessWidget {
         BlocProvider(
           create: (_) => PokemonCubit(repository: PokemonRepository()),
         ),
-         BlocProvider(
+        BlocProvider(
           create: (_) => PokemonSpeciesCubit(repository: PokemonRepository()),
         ),
       ],
@@ -31,13 +28,8 @@ class Pokedex extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Pokédex',
         theme: PokedexTheme.light,
-        darkTheme: PokedexTheme.dark,
-        initialRoute: Routes.splash,
-        routes: {
-          Routes.splash: (context) => const PokemonSplashPage(),
-          Routes.home: (context) => const PokemonHomePage(),
-          Routes.pokemonDetail: (context) => const PokemonDetailPage(),
-        },
+       home: const PokemonSplashPage(),
+       
       ),
     );
   }
